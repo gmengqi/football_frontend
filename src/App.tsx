1ffcf64b668d97837f08c4ad87547bfb59d6b6e8
gmengqi <1014meng@gmail.com>
2024-09-18 14:51:03 +0800

    combine all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import TeamList from './component/TeamForm';
+import MatchResults from './component/MatchForm';
+import Ranking from './component/Ranking'
+import TeamDetails from './component/TeamDetails';
 import Navbar from './component/NavBar';
 
 function HomePage() {
   return (
     <div> 
-      <h1> Hi </h1>
+    <Ranking /> 
+    <TeamDetails />
     </div>
   );
 }
@@ -16,6 +21,11 @@ function App() {
     <Router>
     <div className="App">
       <Navbar />
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add-match" element={<MatchResults />} />
+          <Route path="/add-team" element={<TeamList />} />
+        </Routes>
     </div>
     </Router>
   );
